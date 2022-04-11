5dba489ef6f3f49827932fa073a06e8c7e8ad450
Kevni Woodside <96272858+Code-With-Kev@users.noreply.github.com>
2022-04-11 16:09:23 -0700

    Initial Commit

---
+++ Banking/BankingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankingApp.swift
+//  Banking
+//
+//  Created by Kevni Woodside on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct BankingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
